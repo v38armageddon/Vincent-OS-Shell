c9b87fdc3c5cb6aed6405b1ae3655a5a844b3683
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2021-07-21 12:58:22 +0200

    Vincent OS Shell
    Ceci est un dérivé du terminal du projet Vincent OS

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Vincent-OS-Shell
- Ceci est un dérivé du programme Terminal du mini système d'exploitation Vincent OS
+Ceci est un dérivé du programme Terminal du mini système d'exploitation Vincent OS
+ 
+## Installation
+Une fois que vous avez téléchargé le fichier .zip, dézippez le avec votre logiciel. Pour lancer le logiciel :
+
+Pour les processeurs 32 bits : Vincent OS > Vincent OS > obj > x86 > Debug > Vincent OS Shell.exe
+
+Pour les processeurs 64 bits : Vincent OS > Vincent OS > bin > Debug > Vincent OS Shell.exe
+
+## Information
+Ce projet est un projet Open Source, toute les modifications sont autorisé.
+
+Ceci est un projet qui me servira commme concept. Je pense faire un shell spécial sous Linux mais a voir si je vais y arriver ou non à le faire.
+
+J'espère que le shell va vous plaire.
+
+En souhaitant une agréable journée.
+
+- v38armageddon
---
+++ Vincent OS Shell/Vincent OS Shell.vb
@@ -0,0 +1,36 @@
+﻿Module Shell
+
+    Sub Main()
+        Console.Title = "Vincent OS Shell"
+        Console.Write("Vincent OS [Version Shell Standalone]")
+        Console.WriteLine()
+        Console.WriteLine("Projet : Open Source")
+        Console.WriteLine()
+        cmdloop()
+    End Sub
+
+    Sub cmdloop()
+        Console.WriteLine()
+        Console.Write("vincentOS:\>")
+        Commands.Commands()
+    End Sub
+
+    Sub lssys()
+        Console.Write("Sys/bin")
+        Console.WriteLine()
+        Console.Write("Sys/My Project")
+        Console.WriteLine()
+        Console.Write("Sys/obj")
+        Console.WriteLine()
+        Console.Write("Sys/Commands.vb")
+        Console.WriteLine()
+        Console.Write("Sys/Logo Vincent OS 2.ico")
+        Console.WriteLine()
+        Console.Write("Sys/Vincent OS Shell.vb")
+        Console.WriteLine()
+        Console.Write("Sys/Vincent OS Shell.vbproj")
+        Console.WriteLine()
+        Console.Write("Sys/Vincent OS Shell.vbproj.user")
+        Console.WriteLine()
+    End Sub
+End Module
